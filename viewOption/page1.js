fbc7cf8ab441291b93ae8c0781fa25b7d322389d
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-13 20:09:40 -0400

    install expo-av

---
+++ viewOption/page1.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import { Text, View, 
+  ImageBackground, ScrollView } from 'react-native';
+import {ScaledSheet} from 'react-native-size-matters';
+import {LinearGradient} from 'expo-linear-gradient';
+
+export default function App() {
+
+  const banner = { uri: "https://www.html.am/templates/downloads/bryantsmith/anoceanofsky/anoceanofsky.jpg" };
+  const bg = { uri: "https://www.html.am/templates/downloads/bryantsmith/anoceanofsky/background.png"}
+  return (
+    <View style={styles.container}>
+        <ImageBackground source={bg}
+          style={styles.bg}>
+      <View style={styles.nav}>
+        {/* Nav bar */}
+        <ScrollView 
+            horizontal={true}
+            showHorizontalScrollIndicator={false}>
+                <Text style={styles.navText}>Home</Text>
+                <Text style={styles.navText}>About</Text>
+                <Text style={styles.navText}>Portfolio</Text>
+                <Text style={styles.navText}>Services</Text>
+                <Text style={styles.navText}>Contact</Text>
+        </ScrollView>
+      </View>
+
+      <View style={styles.bannerBorder}>
+        {/* Banner image */}
+        <ImageBackground source={banner}
+          style={styles.bannerBg}>
+        <Text style={styles.bannerHead}>An Ocean of Sky</Text>
+        <Text style={styles.bannerSubHead}>An XHTML 1.0 Strict Template by Bryant Smith</Text>
+        </ImageBackground>
+      </View>
+
+      <View style={styles.bodyBorder}>
+        {/* Body */}
+        <LinearGradient colors={['#E3EDFC', '#fff']} style={styles.bodyBg}>
+        <ScrollView style={styles.content}>
+          <Text style={styles.head}>The Title of an Article</Text>
+              <Text style={styles.bodyText}>You may use this template on any site, anywhere, for free just please leave the link back to me in the footer. This template validates XHTML Strict 1.0, CSS Validates as well; enjoy</Text>
+              <Text style={styles.bodyText}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer mi. Vivamus sit amet neque vitae sapien bibendum sodales. Curabitur elementum. Duis imperdiet. Donec eleifend porttitor sapien. Praesent leo. Quisque auctor velit sed tellus. Suspendisse potenti. Aenean laoreet imperdiet nunc. Donec commodo suscipit dolor. Aenean nibh. Sed id odio. Aliquam lobortis risus ut felis. Sed vehicula pellentesque quam.</Text>
+              <Text style={styles.bodyText}>Vestibulum augue quam, interdum id, congue semper, convallis non, velit. Quisque augue tortor, tristique ac, scelerisque eget, aliquam id, sem. Aenean lorem. Fusce velit nibh, dapibus quis, laoreet nec, porta a, dui. Nullam ac urna. Proin eget elit. Nunc scelerisque venenatis urna. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce congue, turpis ut commodo mattis, pede erat fringilla tellus, pulvinar suscipit odio lorem sed pede.</Text>
+
+        </ScrollView>
+        </LinearGradient>
+      </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = ScaledSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    // justifyContent: 'center', 
+  },
+  bg:{
+    width: '100%',
+    flex: 1,
+    resizeMode: 'cover'
+  },
+  nav: {
+    flex: 0.1,
+    marginTop: 50, 
+  },
+  navText: {
+    fontFamily: 'Georgia',
+    fontSize: '20@mvs0.3',
+    marginHorizontal: '25@mvs',
+    color: '#CDE2FC'
+  },
+  bannerBorder:{
+    borderWidth: '4@s',
+    borderColor: '#fff'
+  },
+  bannerBg: {
+    width: '100%',
+    height: '150@s',
+    resizeMode: "stretch",
+    padding: 20,
+  },
+  bannerHead:{
+    fontFamily: 'Georgia',
+    fontSize: '25@mvs',
+    color: '#fff'
+  },
+  bannerSubHead:{
+    fontFamily: 'Georgia',
+    fontSize: '13@mvs',
+    color: '#A9C8FA'
+  },
+
+  bodyBg: {
+    padding: 20, 
+    height: '100%'
+  },
+  
+  bodyBorder:{
+    flex: 1,
+    borderWidth: '4@s',
+    marginTop: '15@mvs',
+    marginBottom: 10,
+    borderColor: '#fff'
+    
+  },
+
+  content:{
+    width: '320@s',
+    padding: 15,
+    marginBottom: 10
+  },
+  head:{
+    fontFamily: 'Georgia',
+    fontWeight: 'bold',
+    fontSize: '20@mvs0.5',
+    color: '#1F1603',
+    marginBottom: 10
+  },
+  bodyText: {
+    fontFamily: 'Georgia',
+    fontSize: '14@mvs0.5',
+    marginTop: 5, 
+    marginBottom: 20, 
+    lineHeight: '25@mvs0.2',
+  },
+  
+});
