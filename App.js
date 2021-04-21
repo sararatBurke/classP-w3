45e56a2369b9b6c80a87cadd2694b0ee40c7106b
SB <sb@SARARATs-MacBook-Pro.local>
2021-04-20 23:47:09 -0400

    called ModalSample

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Page1 from './viewOption/page1';
+import ModalSameple from './viewOption/modalSample';
 
 export default function App() {
   return (
-    <Page1 />
+    <ModalSameple />
   );
 }
